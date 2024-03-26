69c073f8590fc32008807027cd5f04dd6fd2d800
Igor Molchanov <66907532+meg4cyberc4t@users.noreply.github.com>
2024-03-26 18:26:54 +0300

    Add docs

--- lib/src/build_settings/adapter_mode.dart
+++ lib/src/build_settings/adapter_mode.dart
@@ -5,6 +5,19 @@
 /// the work of configs and configuring the user for experiments.
 /// {@endtemplate}
 enum VarioqubAdapterMode {
+  /// Using Appmetrica adapters from Varioqub/MetricaAdapter in IOS version
+  /// and com.yandex.varioqub.appmetricaadapter.AppMetricaAdapter
+  /// in android version.
+  ///
+  /// In this case, analytics about the use of experiments and configs
+  /// is collected in Appmetrica.
   appmetrica,
+
+  /// Using zero adapter
+  ///
+  /// In this case, analytics about the use
+  /// of configs is not collected.
+  /// The use of experiments is not considered possible,
+  /// there is no way to identify the user.
   none;
 }
